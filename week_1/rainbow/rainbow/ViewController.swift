503f3e37d0e9924480fc863aa61f8f93cd7145ec
Eric <epan626@gmail.com>
2017-02-09 16:28:41 -0800

    rainbow finished

--- week_1/rainbow/rainbow/ViewController.swift
+++ week_1/rainbow/rainbow/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  aging
+//  rainbow
 //
 //  Created by Eric Pan on 2/9/17.
 //  Copyright © 2017 Eric Pan. All rights reserved.
@@ -9,24 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    let names = ["Eric", "Charlie", "Bryan", "Max", "Tri", "David", "Andy", "Judy", "Sally", "Snickers", "Kit", "Kat"]
+
     @IBOutlet weak var tableView: UITableView!
     
+    let arr = [UIColor(red:1.00, green:0.00, blue:0.00, alpha:1.00), UIColor(red:0.99, green:0.50, blue:0.14, alpha:1.00), UIColor(red:1.00, green:0.99, blue:0.22, alpha:1.00), UIColor(red:0.16, green:0.99, blue:0.18, alpha:1.00), UIColor(red:0.04, green:0.14, blue:0.98, alpha:1.00), UIColor(red:0.50, green:0.06, blue:0.49, alpha:1.00)]
+
     
-    var ages = [String]()
-    var counter = 12
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
-        for _ in 0...counter {
-            if counter != 0 {
-                ages.append(String(Int(arc4random_uniform(100))+1))
-                counter -= 1
-            }
-        }
-        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -36,19 +29,26 @@ class ViewController: UIViewController {
     }
 
 
+
 }
 
-extension ViewController: UITableViewDataSource {
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 110.0
+    }
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return names.count
+        return 6
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
-        cell.textLabel?.text = names[indexPath.row]
-        cell.detailTextLabel?.text = String(ages[indexPath.row]) + " years old"
+            cell.contentView.backgroundColor = arr[indexPath.row]
         return cell
+       
     }
+    
 }
